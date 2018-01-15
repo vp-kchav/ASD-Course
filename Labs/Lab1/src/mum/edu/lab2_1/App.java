2a2e7a9fe1d8a20e8891d96f60212b2d1a251415
Kimtey CHAV <kimtey.chav@gmail.com>
2018-01-15 15:35:08 -0600

    lab1

--- Labs/Lab1/src/mum/edu/lab2_1/App.java
+++ Labs/Lab1/src/mum/edu/lab2_1/App.java
@@ -2,9 +2,12 @@ package mum.edu.lab2_1;
 
 public class App {
 
-	public static void main(String[] args) {
-		Singleton singleton = Singleton.getInstance();
-		singleton.doPrint();
+	public static void main(String[] args) throws InterruptedException {
+		for(int i = 1; i<=1000000; i++) {
+			Thread.sleep(100);
+			Singleton singleton = Singleton.getInstance();
+			singleton.doPrint();
+		}
 	}
 
 }
--- Labs/Lab1/src/mum/edu/lab2_1/Singleton.java
+++ Labs/Lab1/src/mum/edu/lab2_1/Singleton.java
@@ -3,7 +3,7 @@ package mum.edu.lab2_1;
 public class Singleton {
 	private static Singleton uniqueInstance = null; 
 	
-	public static Singleton getInstance() {   
+	public synchronized static Singleton getInstance() {   
 		if (uniqueInstance == null) {
 			uniqueInstance = new Singleton();    
 		}
